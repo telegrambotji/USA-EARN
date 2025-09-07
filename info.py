43e6b369678008fc4540b335d2ef8b0f0df3359e
telegrambotji <telegrambotji@gmail.com>
2025-09-07 08:48:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'SilentXBotz_files')
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type True For Turn On MULTIPLE DB FUNTION 
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+sAIZx9peDdMyZjU9')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/nintendo_rom')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+XdG0JyLDSVY4M2E9')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/mrperfectjikabot')
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/movies_update_zone')
