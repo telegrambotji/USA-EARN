a6ddc3e0c06ce9e122259809de53ec81cb8d8e5a
telegrambotji <telegrambotji@gmail.com>
2025-09-07 08:24:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/f983d857f3c
 FSUB_IMG = (environ.get('FSUB_IMG', 'https://i.ibb.co/cShkPjcZ/x.jpg')).split() 
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1982405151').split()] 
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002625346421').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1003058419174').split()]
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002748763541'))  
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '  -1002839994285'))  
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002715836361'))  
